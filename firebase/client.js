e0921704a3c0f988201885c2d748049c9b0fcab5
JarinsonY <jyadith@gmail.com>
2021-04-24 04:55:11 -0500

    Styled JSX and Login With Firebase

---
+++ firebase/client.js
@@ -0,0 +1,42 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCiauQQJfBvr1VnnfiB4ipF1_X0ru9bc6o",
+    authDomain: "twitdev-89d73.firebaseapp.com",
+    projectId: "twitdev-89d73",
+    storageBucket: "twitdev-89d73.appspot.com",
+    messagingSenderId: "109137943896",
+    appId: "1:109137943896:web:79c258a500fc8f1f1f192d",
+    measurementId: "G-BN7L5YB17B"
+}
+
+/* Condicionar a firebase para que se inicialize solo 1 vez */
+firebase.apps.length === 0 && firebase.initializeApp(firebaseConfig)
+
+const mapUserFromFirebaseAuthToUser = (user) => {
+    const { displayName, email, photoURL } = user
+
+    return {
+        avatar: photoURL,
+        username: displayName,
+        email
+    }
+}
+
+export const onAuthStateChanged = (onChange) => {
+    return firebase
+        .auth()
+        .onAuthStateChanged(user => {
+            const normalizedUser =
+                mapUserFromFirebaseAuthToUser(user)
+            onChange(normalizedUser)
+        })
+}
+
+export const loginWithGitHub = () => {
+    const githubProvider = new firebase.auth.GithubAuthProvider()
+    return firebase
+        .auth()
+        .signInWithPopup(githubProvider)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,86 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
+import AppLayout from '../components/AppLayout'
+import Button from '../components/Button'
+import GitHub from '../components/Icons/GitHub'
+import { loginWithGitHub, onAuthStateChanged } from '../firebase/client'
+import { colors } from '../styles/theme'
 
 export default function Home() {
+
+  const [user, setUser] = useState(undefined)
+
+  useEffect(() => {
+    onAuthStateChanged(setUser)
+  }, [])
+
+  const handleClick = () => {
+    loginWithGitHub()
+      .then(setUser)
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>👨‍💻 TwitDev 👩‍💻</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <AppLayout>
+        <section>
+          <img src="/devter-logo.png" alt="Logo" />
+          <h1>TwitDev</h1>
+          <h2>Talk about development <br /> with developers 👨‍💻👩‍💻</h2>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <div>
+            {
+              user === null &&
+              <Button onClick={handleClick}>
+                <GitHub fill='#fff' width={24} height={24} />
+                Login with GitHub
+              </Button>
+            }
+            {
+              user && user.avatar && <div>
+                <img src={user.avatar} />
+                <strong>{user.username}</strong>
+              </div>
+            }
+          </div>
+        </section>
+      </AppLayout>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <style jsx>{`
+        img {
+          width: 120px;
+        }
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        div {
+          margin-top: 16px;
+        }
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        section {
+          display: grid;
+          height: 100%;
+          place-content: center;
+          place-items: center;
+        }
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        h1 {
+          color: ${colors.secondary};
+          font-weight: 800;
+          margin-bottom: 16px;
+        }
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+        h2 {
+          color: ${colors.primary};
+          font-size: 21px;
+          margin: 0;
+        }
+      `}</style>
+    </>
   )
 }
