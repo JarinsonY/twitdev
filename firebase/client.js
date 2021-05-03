a9d3d1dbcc2026f5371e35d01dd2838e9b3b372b
JarinsonY <jyadith@gmail.com>
2021-05-02 19:19:03 -0500

    Add environment variables, change and privatization of firebase_keys

--- firebase/client.js
+++ firebase/client.js
@@ -1,15 +1,6 @@
 import firebase from "firebase"
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCiauQQJfBvr1VnnfiB4ipF1_X0ru9bc6o",
-  authDomain: "twitdev-89d73.firebaseapp.com",
-  projectId: "twitdev-89d73",
-  storageBucket: "twitdev-89d73.appspot.com",
-  messagingSenderId: "109137943896",
-  appId: "1:109137943896:web:79c258a500fc8f1f1f192d",
-  measurementId: "G-BN7L5YB17B",
-}
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG)
 
 /* Condicionar a firebase para que se inicialize solo 1 vez */
 firebase.apps.length === 0 && firebase.initializeApp(firebaseConfig)
