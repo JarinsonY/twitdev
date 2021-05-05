2da6d49bd13380a4412dd887e87160f97f3b414a
JarinsonY <jyadith@gmail.com>
2021-05-05 15:42:33 -0500

    Change fetch

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -69,7 +69,7 @@ export async function getServerSideProps(context) {
   const { params, res } = context
   const { id } = params
 
-  const apiResponse = await fetch(`http://localhost:3000/api/devits/${id}`)
+  const apiResponse = await fetch(`https://twitdev.vercel.app/api/devits/${id}`)
   if (apiResponse.ok) {
     const props = await apiResponse.json()
     return { props }
