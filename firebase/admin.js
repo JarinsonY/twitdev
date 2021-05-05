bc1d498b7cb55209f88e255decb42610385347c2
JarinsonY <jyadith@gmail.com>
2021-05-05 17:01:28 -0500

    Changed Keys v4

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin")
 
-const serviceAccount = require("./firebase-keys.json")
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEYS)
 
 console.log("Funciona")
 
